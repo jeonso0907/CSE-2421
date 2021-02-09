9069a2db6751dad484e7c0cb0e350630821543df
jeon.193 <jeon.193@cse-sl1.coeit.osu.edu>
2021-02-08 22:58:19 -0500

    prototype 2 done

--- lab3/pointer_proto.c
+++ lab3/pointer_proto.c
@@ -3,12 +3,15 @@
 #include <stdio.h>
 #include "struct.h"
 
-void struct_pointer() {
-	struct Firework *f, Fireworke;
-	printf("Launch Time: %lf\n", f->time);
+void struct_pointer(struct Firework firework) {
+		struct Firework *f = &firework;
+		printf("\nInitialize the struct firework variables\n\n");
+		printf("Launch Time: %lf\nHex code: %X\nFuze time: %lf\nX Position: %lf\nX Velocity: %lf\nY Velocity: %lf\n\n", f->time, f->hex, f->fuze, f->x, f->vx, f->vy);
+
 }
 
 int main() {
-	struct_pointer();
+	struct Firework firework;
+	struct_pointer(firework);
 	return 0;
 }
