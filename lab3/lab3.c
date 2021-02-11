d9db09cc0da120002d829d85689893c81e502d15
jeon.193 <jeon.193@cse-sl4.coeit.osu.edu>
2021-02-10 22:21:31 -0500

    lab3.c done, all prototypes merged

--- lab3/lab3.c
+++ lab3/lab3.c
@@ -1,14 +1,28 @@
 /* Sooyoung Jeon */
 
+#include <stdio.h>
 #include "func.h"
 #include "struct.h"
 
+void run_func(struct Firework *rocket, void (*func_ptr)(struct Firework *f)) {
+		func_ptr(rocket);
+}
+
+void print_struct(struct Firework *f) {
+		printf("\nInitialize the struct firework variables\n\n");
+		printf("Launch Time: %lf\nHex code: %X\nFuse time: %lf\nX Position: %lf\nX Velocity: %lf\nY Velocity: %lf\n\n", f->time, f->hex, f->fuse, f->x, f->vx, f->vy); 
+}
+
+void read_struct(struct Firework *f) {
+		while (scanf("%lf %X %lf %lf %lf %lf\n", &f->time, &f->hex, &f->fuse, &f->x, &f->vx, &f->vy) == 6) {
+				run_func(f, &print_struct);
+		}
+}
+
 int main() {
 
-	double time = 0.0, fuze = 0.0, 
-
-	struct firework f1 = {0};
-	scanf("%lf %X %lf %lf %lf %lf", &f1.time, &f1.hex, &f1.fuze, &f1.x, &f1.vx, &f1.vy);
+		struct Firework f = {0}, *rocket = &f;
+		run_func(rocket, &read_struct);
 
 	return 0;
 }
--- lab3/pointer_proto.c
+++ lab3/pointer_proto.c
@@ -2,12 +2,10 @@
 
 #include <stdio.h>
 #include "struct.h"
-#include "proto.h"
 
 void struct_pointer(struct Firework *f) {
 		printf("\nInitialize the struct firework variables\n\n");
 		printf("Launch Time: %lf\nHex code: %X\nFuse time: %lf\nX Position: %lf\nX Velocity: %lf\nY Velocity: %lf\n\n", f->time, f->hex, f->fuse, f->x, f->vx, f->vy);
-
 }
 
 int main() {
