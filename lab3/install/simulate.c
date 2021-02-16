0aaf758d05b11bb50248d486f343865c0d147925
jeon.193 <jeon.193@cse-sl2.coeit.osu.edu>
2021-02-15 20:23:31 -0500

    lab3 done

---
+++ lab3/install/simulate.c
@@ -0,0 +1,22 @@
+/* Sooyoung Jeon */
+
+#include "struct.h"
+#include "libfire.h"
+#include "debug.h"
+#include "linkedlist.h"
+#include "output.h"
+#include "memory.h"
+#include "memory_func.h"
+
+void call_output(struct Sim *s) {
+		if (TEXT) call_text_output(s);
+		if (GRAPHICS) call_graphic_output(s);
+}
+
+void run_simulate(struct Sim *s) {
+		int i = 0;
+		s->dt = least_struct(s, &get_least_dt);
+		while (some_struct(s, &is_not_null)) {
+				call_output(s);
+		}
+}
